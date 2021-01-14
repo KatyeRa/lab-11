4973ef56d26566d09f3e258adc46308f5fdc3cb2
Екатерина Ракитова <you@example.com>
2021-01-14 13:58:46 +0300

    jqery

--- lab11script.js
+++ lab11script.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
   $("#img").click(function () {
-    $("#left").toggle(1000);
+    $(".left").toggle(1000);
   })
-  
+  $(".left").hover(function () {
+        $("left").css("opacity", "1")
+    }, function () {
+        $("left").css("opacity", ".5")
+  });
+  $("ul").click(function () {
+       $(this).toggleClass("highlight");
+     });
+
+  $("#tabl").click(function() {
+    $("#tab").fadeOut();
+  });
+  $("#tabll").click(function() {
+    $("#tab").fadeIn();
+  });
+
+  $("#flip").click(function() {
+    $("#panel").slideDown("slow");
+  });
+
 })
